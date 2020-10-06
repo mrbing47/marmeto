39a3e53d54d4c5818dc1d3b452a604c1e65db0fa
Sarthik Garg <sarthikgarg01@gmail.com>
2020-10-06 17:06:23 +0530

    Added some changes

--- script.js
+++ script.js
@@ -5,16 +5,21 @@ const pincode = document.getElementById("pincode");
 const checkAvailabiltu = document.getElementById("check");
 const addToCart = document.getElementById("add-to-cart");
 let quantity = 1;
+const price = 2599;
 let userPincode = "";
 
 addQty.addEventListener("click", (e) => {
 	e.preventDefault();
 	currQty.innerText = ++quantity;
+	addToCart.innerHTML = `Add to Cart ₹${price * quantity}`;
 });
 
 removeQty.addEventListener("click", (e) => {
 	e.preventDefault();
-	if (quantity > 1) currQty.innerText = --quantity;
+	if (quantity > 1) {
+		currQty.innerText = --quantity;
+		addToCart.innerHTML = `Add to Cart ₹${price * quantity}`;
+	}
 });
 
 pincode.addEventListener("change", (e) => {
